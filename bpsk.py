0f93c289ee8ebec0aa168cb26ab8eebe2b2c3981
michihiro imaoka <imaoca@gmail.com>
2022-04-13 02:59:55 +0900

    Update bpsk.py

--- bpsk.py
+++ bpsk.py
@@ -1,1 +1,11 @@
+import time
+import smbus
 
+I2C = smbus.SMBus(1)
+while True:
+        i = I2C.read_byte_data(0x60, 16)
+        I2C.write_byte_data(0x60, 16, i | 0x10) # CLK0_INV=1
+        time.sleep(0.02)
+        i = I2C.read_byte_data(0x60, 16)
+        I2C.write_byte_data(0x60, 16, i & 0xEF) # CLK0_INV=0
+        time.sleep(0.02)
