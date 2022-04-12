1efe0f841dee8eecc80b5d696e27d0dc959a1c48
michihiro imaoka <imaoca@gmail.com>
2022-04-13 03:04:50 +0900

    Update carrier.py

--- carrier.py
+++ carrier.py
@@ -1,1 +1,13 @@
+import board
+import busio
+import time
 
+import adafruit_si5351
+
+i2c = busio.I2C(board.SCL, board.SDA)
+si5351 = adafruit_si5351.SI5351(i2c)
+
+si5351.pll_a.configure_integer(36)
+si5351.pll_b.configure_fractional(24, 2, 3)
+si5351.clock_0.configure_integer(si5351.pll_a, 30)
+si5351.outputs_enabled = True
